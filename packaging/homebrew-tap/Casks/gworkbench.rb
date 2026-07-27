bca6ce36b9e06ad8be23600fecb4cff844e63a84
life2you <henry@henglin.com>
2026-07-27 15:29:26 +0800

    Update Homebrew cask for v0.1.6

--- packaging/homebrew-tap/Casks/gworkbench.rb
+++ packaging/homebrew-tap/Casks/gworkbench.rb
@@ -1,14 +1,14 @@
 cask "gworkbench" do
-  version "0.1.3"
+  version "0.1.6"
 
   on_arm do
-    sha256 "ce25449c9de33a5ac920231a16eb094deb863f5318a6e23052472d3d4dee0740"
-    url "https://github.com/life2you/gworkbench/releases/download/v0.1.3/GWorkbench-macos-arm64-v0.1.3.zip"
+    sha256 "107bb4c76084be6aae490ce39dd590c8eb4bc0847efdb46b0b90b0f681f915a2"
+    url "https://github.com/life2you/gworkbench/releases/download/v0.1.6/GWorkbench-macos-arm64-v0.1.6.zip"
   end
 
   on_intel do
-    sha256 "f87bcb58094a2de362544265e47db9eab24649c7ece78b14c8737fcfd961ced7"
-    url "https://github.com/life2you/gworkbench/releases/download/v0.1.3/GWorkbench-macos-x86_64-v0.1.3.zip"
+    sha256 "566ec31603f87d55d37628a7b9bf7c345d60ef0251c2971006d373ac95e9c13b"
+    url "https://github.com/life2you/gworkbench/releases/download/v0.1.6/GWorkbench-macos-x86_64-v0.1.6.zip"
   end
 
   name "GWorkbench"
